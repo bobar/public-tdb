53cbb532b59ac31719eff909ae4a742c790308e8
Thierry Deo <thierry.deo@booking.com>
2017-06-14 16:58:24 +0200

    Use session cookie_store for persistence on dyno restart

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cache_store
+Rails.application.config.session_store :cookie_store, expire_after: 1.day
