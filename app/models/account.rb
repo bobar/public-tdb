413aba217519cf7d9828fb4b49c7ae250f0b668a
Thierry Deo <thierry.deo@booking.com>
2017-06-15 15:44:34 +0200

    Improve receiver display

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,8 @@ class Account < ActiveRecord::Base
   has_many :transactions, class_name: :Transaction, foreign_key: :id
 
   DEFAULT_BANK_TRIGRAMME = 'BOB'.freeze
+
+  def full_name
+    "#{first_name} #{name}"
+  end
 end
