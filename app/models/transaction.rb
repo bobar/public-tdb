6b5ccc2f12c3f7096e6e73aeec5f73b853c3b4d6
Thierry Deo <thierry.deo@booking.com>
2017-09-02 16:07:48 +0200

    Fix displaying of transaction recipient

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :buyer, class_name: :Account, foreign_key: :id
-  belongs_to :receiver, class_name: :Account, foreign_key: :id2
+  belongs_to :buyer, class_name: :Account, foreign_key: :buyer_id
+  belongs_to :receiver, class_name: :Account, foreign_key: :receiver_id
 end
